c1c7c40ac75a919acdc61130b24eb48297ae2110
notvalproate <sushruth127@gmail.com>
2026-06-26 00:07:21 +0530

    Added reverse bits solution, not in place

--- BinarySolutions.cpp
+++ BinarySolutions.cpp
@@ -16,6 +16,21 @@ public:
         return count;
     }
 
+    static std::vector<int> counting_bits(int n) {
+        std::vector<int> counts(n + 1);
+        int powerOfTwo = 1;
+
+        for(int i = 1; i <= n; i++) {
+            if (powerOfTwo * 2 == i) {
+                powerOfTwo = i;
+            }
+
+            counts[i] = counts[i - powerOfTwo] + 1;
+        }
+
+        return counts;
+    }
+
     static int sum_of_two(int a, int b) {
         while (b != 0) {
             int c = a & b;
@@ -39,6 +54,16 @@ public:
 
         return xor1 ^ xor2;
     }
+
+    static int reverse_bits(int a) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = a & 0b1;
+            a = a >> 1;
+            result = (result << 1) | bit;
+        }
+        return result;
+    }
 };  
 
 class BinarySolvers {
@@ -50,6 +75,19 @@ public:
         std::cout << "\nSet Bit Count:\n";
         std::cout << "A: " << a << std::endl;
         std::cout << "Answer: " << ans << std::endl;
+    }
+
+    static void counting_bits_solver() {
+        int n = 5;
+        auto ans = BinarySolutions::counting_bits(5);
+    
+        std::cout << "\nCounting Bits Till N:\n";
+        std::cout << "N: " << n << std::endl;
+        std::cout << "Answer: ";
+        for (int v : ans) {
+            std::cout << v << " ";
+        }
+        std::cout << std::endl;
     }
 
     static void sum_of_two_solver() {
@@ -72,11 +110,22 @@ public:
         }
         std::cout << "\nAnswer: " << ans << std::endl;
     }
+
+    static void reverse_bits_solver() {
+        int n = 43261596;
+        int ans = BinarySolutions::reverse_bits(n);
+
+        std::cout << "\nReverse Bits Solution:\n";
+        std::cout << "N: " << n << std::endl;
+        std::cout << "Answer: " << ans << std::endl;
+    }
 };
 
 int main() {
-    BinarySolvers::missing_number_solver();
-    BinarySolvers::sum_of_two_solver();
     BinarySolvers::set_bit_count_solver();
+    BinarySolvers::counting_bits_solver();
+    BinarySolvers::sum_of_two_solver();
+    BinarySolvers::missing_number_solver();
+    BinarySolvers::reverse_bits_solver();
     return 0;
 }
