3d237851b2f4ce4482b5ac854deb63fa79d841b5
notvalproate <sushruth127@gmail.com>
2026-06-25 12:57:23 +0530

    Added sum of two integers solution

--- BinarySolutions.cpp
+++ BinarySolutions.cpp
@@ -7,6 +7,15 @@
 
 class BinarySolutions {
 public:
+    static int sum_of_two(int a, int b) {
+        while(b != 0) {
+            int c = a & b;
+            a = a ^ b;
+            b = c << 1;
+        }
+        return a;
+    }
+
     static int missing_number(const std::vector<int>& values) {
         size_t expected = values.size() + 1;
         int xor1 = 0, xor2 = 0;
@@ -25,6 +34,15 @@ public:
 
 class BinarySolvers {
 public:
+    static void sum_of_two_solver() {
+        int a = 5, b = 11;
+        auto ans = BinarySolutions::sum_of_two(a, b);
+
+        std::cout << "\nSum of Two:\n";
+        std::cout << "A: " << a << ", B: " << b << std::endl;
+        std::cout << "Answer: " << ans << std::endl;
+    }
+
     static void missing_number_solver() {
         std::vector<int> values = {2, 3, 6, 7, 8, 1, 4};
         auto ans = BinarySolutions::missing_number(values);
@@ -40,5 +58,6 @@ public:
 
 int main() {
     BinarySolvers::missing_number_solver();
+    BinarySolvers::sum_of_two_solver();
     return 0;
 }
